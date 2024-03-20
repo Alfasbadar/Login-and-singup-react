919fa9adc8f475e00efacfea6e6419a4c24a9d09
Alfasbadar <4lfasbadar@gmail.com>
2024-03-20 18:45:30 +0530

    Trying to Add a product.

---
+++ src/components/Distribution/Distribution.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Distribution() {
+  return (
+    <div>
+      <h1>distribution</h1>
+    </div>
+  )
+}
+
+export default Distribution
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,29 +1,52 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from '../NavBar/NavBar';
-import Shop from '../Shop/Shop';
-import Items from '../Items/Items';
-import Billing from '../Billing/Billing';
-import Analytics from '../Analytics/Analytics';
-import About from '../About/About';
+import Products from '../Products/Products';
+import Inventory from '../Inventory/Invenetory';
+import Orders from '../Orders/Orders';
+import Stores from '../Stores/Stores';
+import Customers from '../Customers/Customers';
+import Dashboard from '../Dashboard/Dashboard'
+import Distribution from '../Distribution/Distribution';
+
+import { getAuth,signOut } from 'firebase/auth';
 
 const Home = ({ user }) => {
   console.log(user);
+  const handleLogoutClick = async() => {
+    console.log("Logout text click")
+    try{
+      const auth = getAuth();
+      await  signOut(auth);
+      Navigate('/home');
+    }catch(error){
+      console.log('Error Logging out:',error.message);
+    }
+  }
   return (
     <div className="home-container">
-      <div className="navbar-container">
-        <Navbar />
+      <div className="action-bar">
+        <div className="company-name">Company Name</div>
+        <div className="action-buttons">
+          <div className="profile-button">Profile</div>
+          <div className="logout-button" onClick={handleLogoutClick}>Logout</div>
+        </div>
       </div>
       <div className="content">
-        <Routes>
-          {/* Render Welcome message for /home */}
-          <Route path="/" element={<h2>Welcome, {user.email}</h2>} />
-          <Route path="/billing" element={<Billing />} />
-          <Route path="/items" element={<Items />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/analytics" element={<Analytics />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="body">
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard/>} />
+            <Route path="/distribution" element={<Distribution />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/stores" element={<Stores />} />
+            <Route path="/customers" element={<Customers />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
